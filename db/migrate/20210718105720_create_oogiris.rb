1a564bf659480755c2bbcb08d7d0f3814e19df71
rr-rr-re <rr.anonymous.2020@gmail.com>
2021-07-18 19:58:56 +0900

    add oogiri

---
+++ db/migrate/20210718105720_create_oogiris.rb
@@ -0,0 +1,12 @@
+class CreateOogiris < ActiveRecord::Migration[6.1]
+  def change
+    create_table :oogiris do |t|
+      t.string :title
+      t.string :oogiri
+      t.string :description
+      t.string :image_url
+
+      t.timestamps
+    end
+  end
+end
