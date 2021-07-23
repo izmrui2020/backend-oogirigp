6b97251b23d1cbf80fb9f5a4661fb17cf4e3ed96
rr-rr-re <rr.anonymous.2020@gmail.com>
2021-07-23 21:35:32 +0900

    環境変数がうまく使えてない

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,18 +1,18 @@
-unless Rails.env.development? || Rails.env.test?
-  CarrierWave.configure do |config|
-    config.fog_credentials = {
-      :provider:              => 'AWS',
-      :aws_access_key_id:     => ENV['AWS_ACCESS_KEY_ID'],
-      :aws_secret_access_key: => ENV['AWS_SECRET_ACCESS_KEY'],
-      :region:                => ENV['AWS_REGION']
-      #:path_style             => true,
-    }
+# unless Rails.env.development? || Rails.env.test?
+#   CarrierWave.configure do |config|
+#     config.fog_credentials = {
+#       :provider:              => 'AWS',
+#       :aws_access_key_id:     => ENV['AWS_ACCESS_KEY_ID'],
+#       :aws_secret_access_key: => ENV['AWS_SECRET_ACCESS_KEY'],
+#       :region:                => ENV['AWS_REGION']
+#       #:path_style             => true,
+#     }
 
-    # S3のURLに直アクセス禁止
-    # config.fog_public = false
+#     # S3のURLに直アクセス禁止
+#     # config.fog_public = false
 
-    #config.fog_attributes = {'Cache-Control' => 'public, max-age=86400'}
-    config.fog_directory  = ENV['AWS_S3_BUCKET']
-    config.cache_storage = :fog
-  end
-end
+#     #config.fog_attributes = {'Cache-Control' => 'public, max-age=86400'}
+#     config.fog_directory  = ENV['AWS_S3_BUCKET']
+#     config.cache_storage = :fog
+#   end
+# end
