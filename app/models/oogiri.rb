6bbd9590880f9babcfbc2333cdbf86edbeb1642a
rr-rr-re <rr.anonymous.2020@gmail.com>
2021-07-24 03:56:00 +0900

    pagenatione

--- app/models/oogiri.rb
+++ app/models/oogiri.rb
@@ -1,3 +1,6 @@
 class Oogiri < ApplicationRecord
   belongs_to :user
+  validates :user_id, presence: true
+  validates :title, presence: true, length: { maximum: 140 }
+  validates :oogiri, presence: true, length: { maximum: 140 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
 
-  has_many :oogiris, 
-  
+  has_many :oogiris, dependent: :destroy
   mount_uploader :image, ImageUploader
 end
