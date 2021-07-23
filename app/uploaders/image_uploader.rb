2aea6878618f761f2b0b163f322b91f3345927d0
rr-rr-re <rr.anonymous.2020@gmail.com>
2021-07-24 04:42:26 +0900

    start set avator

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,16 +1,27 @@
 class ImageUploader < CarrierWave::Uploader::Base
-  
+  include CarrierWave::RMagick
+
   storage :fog
-  #格納するディレクトリを指定
+
+  # S3のディレクトリ名
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  # 許可する画像の拡張子
   def extension_whitelist
     %w(jpg jpeg gif png)
   end
 
+  # 保存するファイルの命名規則
   def filename
-    original_filename if original_filename
+    "#{secure_token}.#{file.extension}" if original_filename.present?
+  end
+
+  protected
+  # 一意となるトークンを作成
+  def secure_token
+     var = :"@#{mounted_as}_secure_token"
+     model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
   end
 end
